402a4acfcfebc1441d13cb25ea7f936cbc1a00c1
Rana Sam <ranausama2082001@gmail.com>
2022-01-19 21:50:52 +0500

    group1

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import logo from './logo.svg';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import Nav from './components/navbar/Nav'
+import Search from './components/group1/Search';
 function App() {
   return (
+    <>
     <Nav />
+    <Search />
+    </>
   );
 }
 
---
+++ src/components/group1/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './group1.css'
+export default function Group1() {
+    return (
+        <div className='row searchgroup'>
+            <div className='col-1'></div>
+            <div className='col-5'>
+                <h1>Search For Homes In Your Neighborhood</h1>
+                </div>
+            <div className='col-5'></div>
+            <div className='col-1'></div>
+        </div>
+    )
+}
