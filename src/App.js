329cb1fa8605f0273d78c00013271c29405fc341
Rana Sam <ranausama2082001@gmail.com>
2022-01-24 20:25:20 +0500

    formate document

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import './App.css';
-import Nav from './components/navbar/Nav'
-import Search from './components/group1/Search';
-import Choose from './components/group2/Choose';
-import Feature from './components/group3/Feature';
-import News from './components/group5/News';
-import Footer from './components/group6/Footer';
-import Test from './components/group4/Test';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Nav from "./components/navbar/Nav";
+import Search from "./components/group1/Search";
+import Choose from "./components/group2/Choose";
+import Feature from "./components/group3/Feature";
+import News from "./components/group5/News";
+import Footer from "./components/group6/Footer";
+import Test from "./components/group4/Test";
 function App() {
   return (
     <>
-    <Nav />
-    <Search />
-    <Choose />
-    <Feature />
-    <Test />
-    <News />
-    <Footer />
+      <Nav />
+      <Search />
+      <Choose />
+      <Feature />
+      <Test />
+      <News />
+      <Footer />
     </>
   );
 }
